b019eeab3456b77e79aa3d9b7a4ff9fdc10cd470
antoinecontreras <127742345+antoinecontreras@users.noreply.github.com>
2023-11-03 16:39:50 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,35 +1,55 @@
 let myShader;
 let img;
+let animation = false;
+let freq;
+let amp;
 console.log("ANTOINE CONTRERAS DEVELOPPEMENT WITH DIPLOMATIE STUDIO ");
 function preload() {
-  myShader = loadShader('shader/shader.vert', 'shader/shader.frag');
+	myShader = loadShader("shader/shader.vert", "shader/shader.frag");
 }
 const target = document.querySelector("#canvas-container");
 function setup() {
-  let myCanvas = createCanvas(target.offsetWidth, target.offsetHeight, WEBGL);
-  myCanvas.parent("canvas-container");
-  textureWrap(CLAMP);
-  img = loadImage('./logo/logo.png');
-  
-  shader(myShader);
-  
-  myShader.setUniform('tex', img);
-  noStroke();
-}
+	let myCanvas = createCanvas(target.offsetWidth, target.offsetHeight, WEBGL);
+	myCanvas.parent("canvas-container");
+	textureWrap(CLAMP);
+	img = loadImage("./logo/logo.png");
+	shader(myShader);
+	myShader.setUniform("tex", img);
+	noStroke();
 
+  if(checkMobile()){
+    freq = map(300, 0, width, 0, 27.0);
+		amp = map(300, 0, height, 0, 0.07);
+  }else{
+    animation=true;
+  }
+}
+function mouseMoved(){
+  if (animation !== false) {
+
+		freq = map(mouseX, 0, width, 0, 27.0);
+		amp = map(mouseY, 0, height, 0, 0.07);
+	}
+}
 function draw() {
 
-  
-  let freq = map(mouseX, 0, width, 0, 27.0);
-  let amp = map(mouseY, 0, height, 0, 0.07);
+	
 
-  myShader.setUniform('frequency', freq);
-  myShader.setUniform('amplitude', amp);
-  myShader.setUniform('time', frameCount * 0.008);
+	myShader.setUniform("frequency", freq);
+	myShader.setUniform("amplitude", amp);
+	myShader.setUniform("time", frameCount * 0.008);
 
-  rect(0,0,width, height);
+	rect(0, 0, width, height);
 }
 
 function windowResized() {
-  resizeCanvas(target.offsetWidth, target.offsetHeight);
+	resizeCanvas(target.offsetWidth, target.offsetHeight);
+}
+function checkMobile() {
+	let details = navigator.userAgent;
+
+	let regexp = /android|iphone|kindle|ipad/i;
+
+	let isMobileDevice = regexp.test(details);
+	return isMobileDevice;
 }
