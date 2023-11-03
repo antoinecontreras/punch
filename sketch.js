6b58177209715d3c16e348092c12f61100ba8277
antoinecontreras <127742345+antoinecontreras@users.noreply.github.com>
2023-11-03 15:54:48 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+let myShader;
+let img;
+
+function preload() {
+  myShader = loadShader('shader/shader.vert', 'shader/shader.frag');
+}
+const target = document.querySelector("#canvas-container");
+console.log(target, target.offsetWidth);
+function setup() {
+  let myCanvas = createCanvas(target.offsetWidth, target.offsetHeight, WEBGL);
+  myCanvas.parent("canvas-container");
+  textureWrap(CLAMP);
+  img = loadImage('./logo/logo.png');
+  
+  shader(myShader);
+  
+  myShader.setUniform('tex', img);
+  noStroke();
+}
+
+function draw() {
+
+  
+  let freq = map(mouseX, 0, width, 0, 27.0);
+  let amp = map(mouseY, 0, height, 0, 0.07);
+
+  myShader.setUniform('frequency', freq);
+  myShader.setUniform('amplitude', amp);
+  myShader.setUniform('time', frameCount * 0.008);
+
+  rect(0,0,width, height);
+}
+
+function windowResized() {
+  resizeCanvas(target.offsetWidth, target.offsetHeight);
+}
